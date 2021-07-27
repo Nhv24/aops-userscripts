9b80df1fea4222b66fd2c0ba26b536c6bd5c67ee
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-07-27 17:10:53 -0600

    Fix namespace

--- hide-alcumus.user.js
+++ hide-alcumus.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Hide Alcumus
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Nhv24
 // @version      0.1
 // @description  Hides Alcumus ratings (self ratings)
 // @author       player01/Nhv24
 // @match        https://artofproblemsolving.com/alcumus/*
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 $('head').append('<style>.alc--score{display:none;}</style>')
